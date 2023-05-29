89775ce50b9a0904b53700ecbd25d147ec43d145
micheal_gashaw <88850718+young-anakin@users.noreply.github.com>
2023-05-29 18:06:34 +0300

    Add files via upload

---
+++ config/connect.php
@@ -0,0 +1,11 @@
+<?php
+session_start();
+//constants
+define('LOCALHOST','localhost');
+define('DB_USERNAME','root');
+define('DB_PASSWORD','');
+define('DB_NAME', 'brightanvil');
+
+$conn = mysqli_connect(LOCALHOST,DB_USERNAME,DB_PASSWORD) or die(mysqli_error);
+$db_select = mysqli_select_db($conn, DB_NAME) or die(mysqli_error());
+?>
